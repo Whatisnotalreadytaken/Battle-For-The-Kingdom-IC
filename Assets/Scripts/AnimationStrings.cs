ec48d02fd1e3889d4101fdebfef3ccd8ee09f980
Whatisnotalreadytaken <165277514+Whatisnotalreadytaken@users.noreply.github.com>
2024-09-19 13:15:42 +0700

    First One
    First one

---
+++ Assets/Scripts/AnimationStrings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    internal class AnimationStrings
+    {
+    internal static string IsMoving = "IsMoving";
+    internal static string IsSliding = "IsSliding";
+}
+
